bb242534f403f85d2ce126c548784846991e0ed1
Anirudh <anirudh.formal88@gmail.com>
2026-02-07 23:41:23 +0530

    Build editable code editor interface

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,39 @@
 import "./App.css";
+import "prismjs/themes/prism-tomorrow.css";
+import prism from "prismjs";
+import { useEffect, useState } from "react";
+import Editor from "react-simple-code-editor";
 
 function App() {
+  const [code, setCode] = useState(`function sum(){
+  return 1 + 1;
+}`);
+
+  useEffect(() => {
+    prism.highlightAll();
+  }, []);
+
   return (
     <>
       <main>
         <div className="left">
-          <div className="code"></div>
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={(code) => setCode(code)}
+              highlight={(code) =>
+                prism.highlight(code, prism.languages.javascript, "javascript")
+              }
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%",
+              }}
+            />
+          </div>
           <div className="review">Review</div>
         </div>
         <div className="right"></div>
