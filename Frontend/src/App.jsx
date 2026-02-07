035b89eeab8dda6623b97dda117d4f1d783ed5bf
Anirudh <anirudh.formal88@gmail.com>
2026-02-07 22:35:12 +0530

    Create initial code review interface

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code"></div>
+          <div className="review">Review</div>
+        </div>
+        <div className="right"></div>
+      </main>
+    </>
+  );
+}
+
+export default App;
